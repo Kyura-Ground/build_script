be2dd23f240aaabe0db9d193ce77685695a6b51f
Kyura <240605110116@student.uin-malang.ac.id>
2025-12-22 22:45:12 +0700

    Update local_manifest branch and lunch target

--- script.sh
+++ script.sh
@@ -20,7 +20,7 @@ echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone https://github.com/Kyura-Ground/local_manifests.git -b Awaken .repo/local_manifests
+git clone https://github.com/Kyura-Ground/local_manifests.git -b Awaken13 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -41,4 +41,4 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 #build
-lunch awaken_X00TD-user && make installclean && mka bacon
+lunch awaken_X00T-user && make installclean && mka bacon
