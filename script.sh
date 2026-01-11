c6c9b34bd7d4763a4761f5b5fa32745dff449bed
Kyura <240605110116@student.uin-malang.ac.id>
2026-01-11 13:48:47 +0700

    script: Update vendor directory with new repository
    Remove old vendor directory and clone new vendor repository.

--- script.sh
+++ script.sh
@@ -30,6 +30,9 @@ echo "============="
 echo "Sync success"
 echo "============="
 
+rm -rf vendor/lineage
+git clone https://github.com/ikwfahmi/vendor_evolution -b bq vendor/lineage
+
 # Export
 export BUILD_USERNAME=kyura
 export BUILD_HOSTNAME=crave
