24c5df779be2c4c9324f7dc47dfabfb02e495665
Kyura <240605110116@student.uin-malang.ac.id>
2026-01-12 11:29:48 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,13 +13,13 @@ rm -rf vendor/asus
 # echo "============="
 
 #repo init
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
-"=================="
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone -b Cr-16 https://github.com/ikwfahmi/local_manifests.git .repo/local_manifests
+git clone -b Infinity-16 https://github.com/ikwfahmi/local_manifests.git .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -40,4 +40,4 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 #build
-lunch lineage_X00TD-bp3a-user && make installclean && mka bacon
+lunch infinity_X00TD-user && make installclean && m bacon
