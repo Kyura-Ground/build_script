76b532ec5c063f0d220daa6303bc358a0afbf9c4
Kyura <240605110116@student.uin-malang.ac.id>
2026-01-17 17:15:48 +0700

    Update repo branch and lunch target in script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ rm -rf vendor/asus
 # echo "============="
 
 #repo init
-repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
 "=================="
 echo "Repo init success"
 echo "=================="
@@ -43,4 +43,4 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 #build
-lunch lineage_X00TD-bp3a-userdebug && make installclean && m evolution
+lunch lineage_X00TD-bp4a-userdebug && make installclean && m evolution
