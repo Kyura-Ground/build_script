238dbac14c900ecc26066f7e26d0849f129eb2f9
Kyura <240605110116@student.uin-malang.ac.id>
2026-01-29 11:59:00 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -31,7 +31,7 @@ rm -rf "${remove_lists[@]}"
 # echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16-QPR0 -g default,-mips,-darwin,-notdefault
 "=================="
 echo "Repo init success"
 echo "=================="
