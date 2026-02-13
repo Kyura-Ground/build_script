9a4590377d50b812410c8c1626679ec414b78219
Kyura <240605110116@student.uin-malang.ac.id>
2026-02-13 19:26:19 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,13 +17,13 @@ rm -rf vendor/voltage-priv/keys
 # echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr2 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone --depth=1 https://github.com/ikwfahmi/local_manifests.git -b Clover-16 .repo/local_manifests
+git clone --depth=1 https://github.com/ikwfahmi/local_manifests.git -b Infinity-16 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -42,13 +42,9 @@ if [ -d kernel/asus/sdm660 ]; then
 fi
 echo "======= RKSU done ======"
 
-rm -rf device/clover/sepolicy
-git clone --depth=1 https://github.com/Kyura-Ground/device_clover_sepolicy.git device/clover/sepolicy
+rm -rf device/lineage/sepolicy
+git clone --depth=1 https://github.com/Kyura-Ground/device_infinity_sepolicy.git -b 16 device/lineage/sepolicy
 echo "======= sepolicy done ======"
-
-rm -rf build/make
-git clone --depth=1 https://github.com/Kyura-Ground/build_make.git build/make
-echo "======= make done ======"
 
 # Export
 export BUILD_USERNAME=kyura
@@ -60,12 +56,12 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 rm -rf vendor/evolution-priv/keys
-git clone --depth=1 https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
+git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
 cd vendor/evolution-priv/keys
 ./keys.sh
 cd -
 
 #build
-lunch clover_X00TD-bp4a-userdebug
+lunch infinity_X00TD-user
 make installclean
-mka clover
+m bacon
