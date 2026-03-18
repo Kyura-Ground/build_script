345748309d907ac10b296ddb1dabc7cea5919c0d
Kyura <mikhwanfahmi4@gmail.com>
2026-03-18 23:01:48 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -38,13 +38,13 @@ echo "lib6 >> lib5  "
 echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/HertzifyOS/android_manifest.git -b 16 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone --depth=1 https://github.com/ikwfahmi/local_manifests.git -b Infinity-16 .repo/local_manifests
+git clone --depth=1 https://github.com/ikwfahmi/local_manifests.git -b hertzify .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -80,12 +80,12 @@ echo "========================"
 echo " Starting Build: VANILLA"
 echo "========================"
 # Setup untuk perangkat
-lunch infinity_X00TD-user
+lunch hertzify_X00TD-bp4a-user
 make installclean
-m bacon
+mka bacon
 
 # Upload VANILLA Build
-for file in out/target/product/X00TD/Project_Infinity*.zip; do
+for file in out/target/product/X00TD/hertzify*.zip; do
     if [ -f "$file" ]; then
         echo "Mulai mengupload VANILLA: $file"
         curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
