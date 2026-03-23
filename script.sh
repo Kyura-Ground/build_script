64f3c6d395963c834a90123df8754e155e30d308
Kyura <mikhwanfahmi4@gmail.com>
2026-03-23 07:14:46 +0700

    Update repository URLs and build commands in script.sh

--- script.sh
+++ script.sh
@@ -38,13 +38,13 @@ echo "lib6 >> lib5  "
 echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Nusantara-Fan-Edition/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone --depth=1 https://github.com/ikwfahmi/local_manifests.git -b Cr-10 .repo/local_manifests
+git clone --depth=1 https://github.com/ikwfahmi/local_manifests.git -b main .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -56,12 +56,12 @@ echo "Sync success"
 echo "============="
 
 # setup KernelSU
-if [ -d kernel/asus/sdm660 ]; then 
-cd kernel/asus/sdm660
-curl -LSs "https://raw.githubusercontent.com/Sorayukii/KernelSU-Next/stable/kernel/setup.sh" | bash -s hookless
-cd ../../..
-fi
-echo "======= RKSU done ======"
+# if [ -d kernel/asus/sdm660 ]; then 
+# cd kernel/asus/sdm660
+# curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s master
+# cd ../../..
+# fi
+# echo "======= RKSU done ======"
 
 # Set up build environment
 export BUILD_USERNAME=kenq
@@ -69,22 +69,23 @@ export BUILD_HOSTNAME=crave
 export TZ="Asia/Jakarta"
 source build/envsetup.sh
 
-# rm -rf vendor/evolution-priv/keys
-# git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
-# cd vendor/evolution-priv/keys
-# ./keys.sh
-# cd ../../..
+rm -rf vendor/evolution-priv/keys
+git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
+cd vendor/evolution-priv/keys
+./keys.sh
+cd ../../..
+
 
 echo "========================"
 echo " Starting Build: VANILLA"
 echo "========================"
 # Setup untuk perangkat
-lunch nad_X00TD-user
+lunch infinity_X00TD-user
 make installclean
-mka nad
+m bacon
 
 # Upload VANILLA Build
-for file in out/target/product/X00TD/Nusantara*.zip; do
+for file in out/target/product/X00TD/Project_Infinity*.zip; do
     if [ -f "$file" ]; then
         echo "Mulai mengupload VANILLA: $file"
         curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
