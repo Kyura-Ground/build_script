d18bcd33e83bc43bfbf671ef9156377133b31926
Kyura <mikhwanfahmi4@gmail.com>
2026-04-05 09:19:40 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -40,13 +40,13 @@ echo "lib6 >> lib5  "
 echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Evolution-X/manifest -b bq2 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Infinity-16 .repo/local_manifests
+git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Evox-16 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -89,12 +89,12 @@ echo "========================"
 echo " Starting Build: GApps"
 echo "========================"
 # Setup untuk perangkat
-lunch infinity_X00TD-user
+lunch lineage_X00TD-bp4a-user
 make installclean
-m bacon
+m evolution
 
 # Upload VANILLA Build
-for file in out/target/product/X00TD/Project_Infinity*.zip; do
+for file in out/target/product/X00TD/EvolutionX*.zip; do
     if [ -f "$file" ]; then
         echo "Mulai mengupload VANILLA: $file"
         curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
