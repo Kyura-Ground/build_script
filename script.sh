cf1cdfd132f6741bcbdf2a57a82caac31f47e527
Kyura <mikhwanfahmi4@gmail.com>
2026-04-20 08:42:57 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -21,7 +21,6 @@ remove_lists=(
 	vendor/voltage-priv/keys
 	hardware/lineage/interfaces
 	hardware/qcom-caf/sdm660/audio
-	packages/apps/HertzifySettings
 )
 
 do_reclone() {
@@ -41,13 +40,13 @@ echo "lib6 >> lib5  "
 echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/halcyonproject/manifest -b 16.2 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Lunaris-AOSP/android -b 16.2 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Hakcyon-16 .repo/local_manifests
+git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Lunaris-16 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -82,22 +81,19 @@ rm -rf hardware/qcom-caf/sdm660/audio
 git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/rsuntk-asus-sdm660/android_hardware_qcom-caf_audio.git hardware/qcom-caf/sdm660/audio
 # git clone --depth=1 -b 16.0 https://github.com/Kyura-Ground/android_build.git build/make
 
-rm -rf build/soong
-git clone --depth=1 https://github.com/Kyura-Ground/build_soong.git build/soong
-
 rm -rf build/make
-git clone --depth=1 https://github.com/Kyura-Ground/build_make.git build/make
+git clone --depth=1 https://github.com/Kyura-Ground/build_lunaris.git build/make
 
 echo "========================"
 echo " Starting Build: GApps"
 echo "========================"
 # Setup untuk perangkat
-lunch halcyon_X00TD-bp4a-user
+lunch lineage_X00TD-bp4a-user
 make installclean
-mka carthage
+m bacon
 
 # Upload VANILLA Build
-for file in out/target/product/X00TD/halcyon*.zip; do
+for file in out/target/product/X00TD/Lunaris*.zip; do
     if [ -f "$file" ]; then
         echo "Mulai mengupload VANILLA: $file"
         curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
