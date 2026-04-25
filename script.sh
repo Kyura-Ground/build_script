0d50c68174cc18f3fb543ffba89aacba98ff253b
Kyura <mikhwanfahmi4@gmail.com>
2026-04-25 08:59:08 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,58 +1,17 @@
-# cleanup
-remove_lists=(
-    .repo/local_manifests
-    device/asus/X00TD
-    device/lineage/sepolicy
-    device/qcom/sepolicy
-    device/qcom/sepolicy-legacy-um
-    device/qcom/sepolicy_vndr/legacy-um
-    external/chromium-webview
-    kernel/asus/sdm660
-    out/target/product/X00TD
-    prebuilts/clang/host/linux-x86
-    packages/modules/Nfc
-    packages/apps/Nfc
-    system/nfc
-    vendor/extras
-    vendor/addons
-    vendor/asus
-    vendor/lineage-priv/keys
-    vendor/evolution-priv/keys
-	vendor/voltage-priv/keys
-	hardware/lineage/interfaces
-	hardware/qcom-caf/sdm660/audio
-)
-
-do_reclone() {
-    rm -rf $3
-    echo "-- Recloning $3 ..."
-    git clone --depth=1 $1 -b $2 $3
-}
-
-echo "-- Removing ${remove_lists[@]}"
-rm -rf "${remove_lists[@]}"
-
-# Symlink libncurses 6 >> 5
-sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
-sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6   /usr/lib/x86_64-linux-gnu/libtinfo.so.5
-echo "============="
-echo "lib6 >> lib5  "
-echo "============="
-
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Lunaris-AOSP/android -b 16.2 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Evolution-X/manifest -b bq2 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Lunaris-16 .repo/local_manifests
+git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b main .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
 #Sync
-[ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 echo "============="
 echo "Sync success"
 echo "============="
@@ -66,8 +25,8 @@ echo "============="
 # echo "======= RKSU done ======"
 
 # Set up build environment
-export BUILD_USERNAME=kenq
-export BUILD_HOSTNAME=crave
+export BUILD_USERNAME=kyura
+export BUILD_HOSTNAME=serverhive
 export TZ="Asia/Jakarta"
 source build/envsetup.sh
 
@@ -81,28 +40,13 @@ rm -rf hardware/qcom-caf/sdm660/audio
 git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/rsuntk-asus-sdm660/android_hardware_qcom-caf_audio.git hardware/qcom-caf/sdm660/audio
 # git clone --depth=1 -b 16.0 https://github.com/Kyura-Ground/android_build.git build/make
 
-rm -rf build/make
-git clone --depth=1 https://github.com/Kyura-Ground/build_lunaris.git build/make
+# rm -rf build/make
+# git clone --depth=1 https://github.com/Kyura-Ground/build_lunaris.git build/make
 
-echo "========================"
-echo " Starting Build: Vanilla"
-echo "========================"
+# echo "========================"
+# echo " Starting Build: Vanilla"
+# echo "========================"
 # Setup untuk perangkat
-lunch lineage_X00TD-bp4a-user
-make installclean
-m bacon
-
-# Upload VANILLA Build
-for file in out/target/product/X00TD/Lunaris*.zip; do
-    if [ -f "$file" ]; then
-        echo "Mulai mengupload VANILLA: $file"
-        curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
-        echo -e "\nUpload selesai untuk $file"
-        
-        # Pindahkan file ke direktori utama agar tidak terupload ulang nanti
-        mv "$file" ./
-        echo "--------------MOVED VANILLA BUILD TO ROOT DIRECTORY--------------"
-    else
-        echo "File tidak ditemukan!"
-    fi
-done
+# lunch lineage_X00TD-bp4a-user
+# make installclean
+# m bacon
