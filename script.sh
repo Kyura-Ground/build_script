91a4623e1d77218de51a84cc6f7ecaee814cdd60
Kyura <mikhwanfahmi4@gmail.com>
2026-05-04 16:19:35 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -41,13 +41,13 @@ echo "lib6 >> lib5  "
 echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/HertzifyOS/android_manifest.git -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Hertzify-16 .repo/local_manifests
+git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Infinity-16 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -83,19 +83,19 @@ git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/rsuntk-asus-sd
 
 rm -rf hardware/lineage/interfaces
 rm -rf hardware/hertzify/interfaces
-git clone --depth=1 -b 16 https://github.com/Kyura-Ground/android_hardware_hertzify_interfaces.git hardware/hertzify/interfaces
+git clone --depth=1 -b lineage-23.2 https://github.com/rsuntk-asus-sdm660/android_hardware_lineage_interfaces.git hardware/lineage/interfaces
 
 echo "========================"
 echo " Starting Build: Vanilla"
 echo "========================"
 
 # Setup untuk perangkat
-lunch hertzify_X00TD-bp4a-user
+lunch infinity_X00TD-user
 make installclean
-mka bacon
+m bacon
 
 # Upload VANILLA Build
-for file in out/target/product/X00TD/HertzifyOS*.zip; do
+for file in out/target/product/X00TD/Project_Infinity-X*.zip; do
     if [ -f "$file" ]; then
         echo "Mulai mengupload VANILLA: $file"
         curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
