d34ceb97a7eba5b95f05f4f0d120268fdbbacac1
Kyura <mikhwanfahmi4@gmail.com>
2026-06-05 09:07:17 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -41,13 +41,13 @@ echo "lib6 >> lib5  "
 echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/halcyonproject/manifest -b 16.2 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Hakcyon-16 .repo/local_manifests
+git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Infinity-16 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -78,29 +78,20 @@ cd vendor/evolution-priv/keys
 ./keys.sh
 cd ../../..
 
-rm -rf build/soong
-git clone --depth=1 https://github.com/Kyura-Ground/build_soong.git build/soong
-
 rm -rf hardware/qcom-caf/sdm660/audio
-git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/Kyura-Ground/android_hardware_qcom_audio.git hardware/qcom-caf/sdm660/audio
-
-rm -rf hardware/qcom-caf/sdm660/display
-git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/Kyura-Ground/android_hardware_qcom_display.git hardware/qcom-caf/sdm660/display
-
-rm -rf hardware/lineage/interfaces
-git clone --depth=1 -b lineage-23.2 https://github.com/Kyura-Ground/android_hardware_lineage_interfaces.git hardware/lineage/interfaces
+git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/SonicBSV/android_hardware_qcom-caf_sdm660_audio.git hardware/qcom-caf/sdm660/audio
 
 echo "========================"
 echo " Starting Build: Vanilla"
 echo "========================"
 
 # Setup untuk perangkat
-lunch halcyon_X00TD-bp4a-user
+lunch infinity_X00TD-user
 make installclean
-mka carthage
+m bacon
 
 # Upload VANILLA Build
-for file in out/target/product/X00TD/halcyon*.zip; do
+for file in out/target/product/X00TD/Project_Infinity-X*.zip; do
     if [ -f "$file" ]; then
         echo "Mulai mengupload VANILLA: $file"
         curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
