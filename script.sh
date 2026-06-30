93265ed58c1269ac4979d9584fab695b7a6bb79b
Kyura <mikhwanfahmi4@gmail.com>
2026-06-30 16:03:12 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,36 @@
-rm -rf .repo/local_manifests/
+# cleanup
+remove_lists=(
+    .repo/local_manifests
+    device/asus/X00TD
+    kernel/asus/sdm660
+    vendor/asus
+    vendor/evolution-priv/keys
+)
+
+do_reclone() {
+    rm -rf $3
+    echo "-- Recloning $3 ..."
+    git clone --depth=1 $1 -b $2 $3
+}
+
+echo "-- Removing ${remove_lists[@]}"
+rm -rf "${remove_lists[@]}"
+
+# Symlink libncurses 6 >> 5
+sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
+sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6   /usr/lib/x86_64-linux-gnu/libtinfo.so.5
+echo "============="
+echo "lib6 >> lib5  "
+echo "============="
 
 #repo init
-repo init -u https://github.com/crdroid-13-fork/android.git -b 13.0 --git-lfs
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/crdroid-13-fork/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #Sync
-/opt/crave/resync.sh
+[ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
@@ -21,14 +44,23 @@ echo "============================"
 echo "Clone X00TD Resources done"
 echo "============================"
 
-# Export
+# Set up build environment
 export BUILD_USERNAME=kyura
 export BUILD_HOSTNAME=crave
-echo "======= Export Done ======"
+export TZ="Asia/Jakarta"
+source build/envsetup.sh
 
-# Set up build environment
-. build/envsetup.sh
-echo "====== Envsetup Done ======="
+# rm -rf vendor/evolution-priv/keys
+# git clone --depth=1 https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
+# cd vendor/evolution-priv/keys
+# ./keys.sh
+# cd ../../..
+
+# rm -rf hardware/qcom-caf/sdm660/audio
+# git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/SonicBSV/android_hardware_qcom-caf_sdm660_audio.git hardware/qcom-caf/sdm660/audio
+
+# rm -rf build/soong
+# git clone --depth=1 -b 16.2 https://github.com/Kyura-Ground/build_soong build/soong
 
 rm -rf hardware/qcom-caf/msm8998/display
 rm -rf hardware/qcom-caf/msm8998/audio
@@ -40,12 +72,22 @@ git clone --depth=1 https://github.com/muralivijay/android_hardware_qcom-caf_sdm
 git clone --depth=1 https://github.com/muralivijay/android_hardware_qcom-caf_sdm660_media -b lineage-20.0-caf-sdm660 hardware/qcom-caf/msm8998/media
 echo "====== cloned 4.19 msm8998 HALS ======="
 
-# Check keys before procced build
-if [ -d "vendor/extra" ]; then
-    echo "✓ Keys found for sign build. Proceeding build..."
-    lunch lineage_X00TD-user && mka clean && mka clobber && mka bacon
-else
-    echo "✗ Error: keys not found Directory 'vendor/extra'."
-    echo "Stopping script execution."
-    exit 1
-fi
+# Setup untuk perangkat
+lunch lineage_X00TD-user
+make installclean
+mka bacon
+
+# Upload VANILLA Build
+for file in out/target/product/X00TD/crDroidAndroid*.zip; do
+    if [ -f "$file" ]; then
+        echo "Mulai mengupload VANILLA: $file"
+        curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
+        echo -e "\nUpload selesai untuk $file"
+        
+        # Pindahkan file ke direktori utama agar tidak terupload ulang nanti
+        mv "$file" ./
+        echo "--------------MOVED VANILLA BUILD TO ROOT DIRECTORY--------------"
+    else
+        echo "File tidak ditemukan!"
+    fi
+done
