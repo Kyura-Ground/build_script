fd9cb3805f6cd79f79f85bbda54954972b494b8c
Kyura <mikhwanfahmi4@gmail.com>
2026-06-30 12:39:57 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,92 +1,51 @@
-# cleanup
-remove_lists=(
-    .repo/local_manifests
-    device/asus/X00TD
-    kernel/asus/sdm660
-    vendor/asus
-    vendor/evolution-priv/keys
-)
-
-do_reclone() {
-    rm -rf $3
-    echo "-- Recloning $3 ..."
-    git clone --depth=1 $1 -b $2 $3
-}
-
-echo "-- Removing ${remove_lists[@]}"
-rm -rf "${remove_lists[@]}"
-
-# Symlink libncurses 6 >> 5
-sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
-sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6   /usr/lib/x86_64-linux-gnu/libtinfo.so.5
-echo "============="
-echo "lib6 >> lib5  "
-echo "============="
+rm -rf .repo/local_manifests/
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/halcyonproject/manifest -b 16.2 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/crdroid-13-fork/android.git -b 13.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
-#local_manifest
-git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Hakcyon-16 .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
-
 #Sync
-[ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
+/opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
 
-# setup KernelSU
-# if [ -d kernel/asus/sdm660 ]; then 
-# cd kernel/asus/sdm660
-# curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s master
-# cd ../../..
-# fi
-# echo "======= XXKSU done ======"
+#local_manifest
+git clone https://github.com/Kyura-Ground/android_device_asus_X00TD-4.19.git -b 13.0 device/asus/X00TD
+git clone https://github.com/muralivijay/android_device_asus_sdm660-common-4.19.git -b 13.0 device/asus/sdm660-common
+git clone https://github.com/muralivijay/proprietary_vendor_asus-4.19.git -b 13.0 vendor/asus
+git clone https://github.com/muralivijay/android_kernel_asus_sdm660-4.19.git -b Ratibor-Rebased-Murali kernel/asus/sdm660-common
+echo "============================"
+echo "Clone X00TD Resources done"
+echo "============================"
 
-# Set up build environment
+# Export
 export BUILD_USERNAME=kyura
 export BUILD_HOSTNAME=crave
-export TZ="Asia/Jakarta"
-source build/envsetup.sh
+echo "======= Export Done ======"
 
-rm -rf vendor/evolution-priv/keys
-git clone --depth=1 https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
-cd vendor/evolution-priv/keys
-./keys.sh
-cd ../../..
+# Set up build environment
+. build/envsetup.sh
+echo "====== Envsetup Done ======="
 
-rm -rf hardware/qcom-caf/sdm660/audio
-git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/SonicBSV/android_hardware_qcom-caf_sdm660_audio.git hardware/qcom-caf/sdm660/audio
+rm -rf hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/audio
+rm -rf hardware/qcom-caf/msm8998/media
+echo "====== Removed legacy msm8998 HALS ======="
 
-rm -rf build/soong
-git clone --depth=1 -b 16.2 https://github.com/Kyura-Ground/build_soong build/soong
+git clone https://github.com/muralivijay/android_hardware_qcom-caf_sdm660_audio -b lineage-20.0-caf-sdm660 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/muralivijay/android_hardware_qcom-caf_sdm660_display -b lineage-20.0-caf-sdm660 hardware/qcom-caf/msm8998/display
+git clone https://github.com/muralivijay/android_hardware_qcom-caf_sdm660_media -b lineage-20.0-caf-sdm660 hardware/qcom-caf/msm8998/media
+echo "====== cloned 4.19 msm8998 HALS ======="
 
-echo "========================"
-echo " Starting Build: Vanilla"
-echo "========================"
-
-# Setup untuk perangkat
-lunch halcyon_X00TD-bp4a-user
-make installclean
-mka carthage
-
-# Upload VANILLA Build
-for file in out/target/product/X00TD/halcyon*.zip; do
-    if [ -f "$file" ]; then
-        echo "Mulai mengupload VANILLA: $file"
-        curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
-        echo -e "\nUpload selesai untuk $file"
-        
-        # Pindahkan file ke direktori utama agar tidak terupload ulang nanti
-        mv "$file" ./
-        echo "--------------MOVED VANILLA BUILD TO ROOT DIRECTORY--------------"
-    else
-        echo "File tidak ditemukan!"
-    fi
-done
+# Check keys before procced build
+if [ -d "vendor/extra" ]; then
+    echo "✓ Keys found for sign build. Proceeding build..."
+    lunch lineage_X00TD-user && mka clean && mka clobber && mka bacon
+else
+    echo "✗ Error: keys not found Directory 'vendor/extra'."
+    echo "Stopping script execution."
+    exit 1
+fi
