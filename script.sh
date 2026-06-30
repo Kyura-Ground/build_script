1f7f9ac6e061593c533a45be24f7c837cfcb5972
Kyura <mikhwanfahmi4@gmail.com>
2026-07-01 04:37:20 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -37,8 +37,8 @@ echo "============="
 
 #local_manifest
 git clone --depth=1 https://github.com/Kyura-Ground/android_device_asus_X00TD-4.19.git -b 13.0 device/asus/X00TD
-git clone --depth=1 https://github.com/muralivijay/android_device_asus_sdm660-common-4.19.git -b 13.0 device/asus/sdm660-common
-git clone --depth=1 https://github.com/muralivijay/proprietary_vendor_asus-4.19.git -b 13.0 vendor/asus
+git clone --depth=1 https://github.com/Kyura-Ground/android_device_asus_sdm660-common-4.19.git -b 13.0 device/asus/sdm660-common
+git clone --depth=1 https://github.com/Kyura-Ground/proprietary_vendor_asus-4.19.git -b 13.0 vendor/asus
 git clone --depth=1 --recursive https://github.com/muralivijay/android_kernel_asus_sdm660-4.19.git -b Ratibor-Rebased-Murali kernel/asus/sdm660-common
 git clone --depth=1 https://github.com/Kyura-Ground/public-keys.git -b main vendor/lineage-priv/keys/
 echo "============================"
@@ -62,16 +62,6 @@ source build/envsetup.sh
 
 # rm -rf build/soong
 # git clone --depth=1 -b 16.2 https://github.com/Kyura-Ground/build_soong build/soong
-
-rm -rf hardware/qcom-caf/msm8998/display
-rm -rf hardware/qcom-caf/msm8998/audio
-rm -rf hardware/qcom-caf/msm8998/media
-echo "====== Removed legacy msm8998 HALS ======="
-
-git clone --depth=1 https://github.com/muralivijay/android_hardware_qcom-caf_sdm660_audio -b lineage-20.0-caf-sdm660 hardware/qcom-caf/msm8998/audio
-git clone --depth=1 https://github.com/muralivijay/android_hardware_qcom-caf_sdm660_display -b lineage-20.0-caf-sdm660 hardware/qcom-caf/msm8998/display
-git clone --depth=1 https://github.com/muralivijay/android_hardware_qcom-caf_sdm660_media -b lineage-20.0-caf-sdm660 hardware/qcom-caf/msm8998/media
-echo "====== cloned 4.19 msm8998 HALS ======="
 
 # Setup untuk perangkat
 lunch lineage_X00TD-user
