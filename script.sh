703d29e0bca5b295727a27fb65bab9534788d857
Kyura <mikhwanfahmi4@gmail.com>
2026-07-18 15:44:27 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 remove_lists=(
     .repo/local_manifests
     device/asus/X00TD
+    device/asus/sdm660-common
     kernel/asus/sdm660
     vendor/asus
     vendor/evolution-priv/keys
@@ -24,16 +25,10 @@ echo "lib6 >> lib5  "
 echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/LineageOS/android.git -b lineage-23.2 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/RisingTechOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
-
-#local_manifest
-git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b lineage-23.2 .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
 
 #Sync
 [ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
@@ -41,13 +36,14 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-# setup KernelSU
-# if [ -d kernel/asus/sdm660 ]; then 
-# cd kernel/asus/sdm660
-# curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s master
-# cd ../../..
-# fi
-# echo "======= XXKSU done ======"
+#local_manifest
+git clone --depth=1 https://github.com/Kyura-Ground/device_asus_X00T.git -b 13.0 device/asus/X00T
+git clone --depth=1 https://github.com/Kyura-Ground/vendor_asus.git -b 13.0 vendor/asus
+git clone --depth=1 https://github.com/Kyura-Ground/android_kernel_asus_sdm660-4.4.git -b lineage-20.0 kernel/asus/sdm660
+git clone --depth=1 https://github.com/Kyura-Ground/public-keys.git -b main vendor/lineage-priv/keys/
+echo "============================"
+echo "Clone X00TD Resources done"
+echo "============================"
 
 # Set up build environment
 export BUILD_USERNAME=kyura
@@ -55,29 +51,25 @@ export BUILD_HOSTNAME=crave
 export TZ="Asia/Jakarta"
 source build/envsetup.sh
 
-rm -rf vendor/evolution-priv/keys
-git clone --depth=1 https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
-cd vendor/evolution-priv/keys
-./keys.sh
-cd ../../..
+# rm -rf vendor/evolution-priv/keys
+# git clone --depth=1 https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
+# cd vendor/evolution-priv/keys
+# ./keys.sh
+# cd ../../..
 
-rm -rf hardware/qcom-caf/sdm660/audio
-git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/SonicBSV/android_hardware_qcom-caf_sdm660_audio.git hardware/qcom-caf/sdm660/audio
+# rm -rf hardware/qcom-caf/sdm660/audio
+# git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/SonicBSV/android_hardware_qcom-caf_sdm660_audio.git hardware/qcom-caf/sdm660/audio
 
-rm -rf build/soong
-git clone --depth=1 -b lineage-23.2 https://github.com/Kyura-Ground/android_build_soong build/soong
-
-echo "========================"
-echo " Starting Build: Vanilla"
-echo "========================"
+# rm -rf build/soong
+# git clone --depth=1 -b 16.2 https://github.com/Kyura-Ground/build_soong build/soong
 
 # Setup untuk perangkat
-lunch lineage_X00TD-bp4a-user
+lunch lineage_X00T-user
 make installclean
 mka bacon
 
 # Upload VANILLA Build
-for file in out/target/product/X00TD/lineage*.zip; do
+for file in out/target/product/X00T/rising*.zip; do
     if [ -f "$file" ]; then
         echo "Mulai mengupload VANILLA: $file"
         curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
