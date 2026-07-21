c192a431cf5ccb8d7dbb7878bd00fec7eb156ccf
Kyura <mikhwanfahmi4@gmail.com>
2026-07-21 09:30:05 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -25,7 +25,7 @@ echo "lib6 >> lib5  "
 echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Rising-LTS/android -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Evolution-X/manifest -b cnb -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -37,21 +37,18 @@ echo "Sync success"
 echo "============="
 
 #local_manifest
-git clone --depth=1 https://github.com/Kyura-Ground/device_asus_X00T.git -b 13.0 device/asus/X00T
-git clone --depth=1 https://github.com/Kyura-Ground/vendor_asus.git -b 13.0 vendor/asus
-git clone --depth=1 https://github.com/Kyura-Ground/android_kernel_asus_sdm660-4.4.git -b lineage-20.0 kernel/asus/sdm660
-git clone --depth=1 https://github.com/Kyura-Ground/public-keys.git -b main vendor/lineage-priv/keys/
+git clone -b lineage-24.0 https://github.com/Kyura-Ground/local_manifests.git .repo/local_manifests --depth=1
 echo "============================"
 echo "Clone X00TD Resources done"
 echo "============================"
 
 # setup KernelSU
-if [ -d kernel/asus/sdm660 ]; then 
-cd kernel/asus/sdm660
-curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s master
-cd ../../..
-fi
-echo "======= XXKSU done ======"
+# if [ -d kernel/asus/sdm660 ]; then 
+#cd kernel/asus/sdm660
+# curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s master
+# cd ../../..
+# fi
+# echo "======= XXKSU done ======"
 
 # Set up build environment
 export BUILD_USERNAME=kyura
@@ -59,11 +56,11 @@ export BUILD_HOSTNAME=crave
 export TZ="Asia/Jakarta"
 source build/envsetup.sh
 
-# rm -rf vendor/evolution-priv/keys
-# git clone --depth=1 https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
-# cd vendor/evolution-priv/keys
-# ./keys.sh
-# cd ../../..
+rm -rf vendor/evolution-priv/keys
+git clone --depth=1 https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
+cd vendor/evolution-priv/keys
+./keys.sh
+cd ../../..
 
 # rm -rf hardware/qcom-caf/sdm660/audio
 # git clone --depth=1 -b lineage-23.2-caf-sdm660 https://github.com/SonicBSV/android_hardware_qcom-caf_sdm660_audio.git hardware/qcom-caf/sdm660/audio
@@ -72,12 +69,12 @@ source build/envsetup.sh
 # git clone --depth=1 -b 16.2 https://github.com/Kyura-Ground/build_soong build/soong
 
 # Setup untuk perangkat
-lunch lineage_X00T-user
+lunch lineage_X00TD-cp2a-user
 make installclean
-mka bacon
+m evolution
 
 # Upload VANILLA Build
-for file in out/target/product/X00T/rising*.zip; do
+for file in out/target/product/X00T/Evolution*.zip; do
     if [ -f "$file" ]; then
         echo "Mulai mengupload VANILLA: $file"
         curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
