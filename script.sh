ed7ae3156fd6e8ec0337e3fc7d7ec08ccc2bb9e0
M. Ikhwan Fahmi <mikhwanfahmi4@gmail.com>
2026-09-16 18:53:00 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -24,13 +24,13 @@ echo "lib6 >> lib5  "
 echo "============="
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/crDroid11/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/VoltageOS/manifest.git -b 17 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b lineage-18.1 .repo/local_manifests
+git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Voltage .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -55,32 +55,28 @@ export BUILD_HOSTNAME=crave
 export TZ="Asia/Jakarta"
 source build/envsetup.sh
 
-# rm -rf vendor/evolution-priv/keys
-# git clone --depth=1 https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
-# cd vendor/evolution-priv/keys
-# ./keys.sh
-# cd ../../..
+rm -rf vendor/evolution-priv/keys
+rm -rf voltage-priv/keys
+git clone --depth=1 https://github.com/VoltageOS/vendor_voltage-priv_keys vendor/voltage-priv/keys
+cd vendor/voltage-priv/keys
+./keys.sh
+cd ../../..
 
-# rm -rf packages/modules/Connectivity
-# git clone --depth=1 https://github.com/Kyura-Ground/android_packages_modules_Connectivity packages/modules/Connectivity
-
-# rm -rf hardware/qcom-caf/sdm660/audio
-# git clone --depth=1 https://github.com/Kyura-Ground/android_hardware_qcom_audio hardware/qcom-caf/sdm660/audio
-
-# rm -rf build/soong
-# git clone --depth=1 https://github.com/Kyura-Ground/android_build_soong build/soong
+rm -rf build/soong
+git clone --depth=1 https://github.com/Kyura-Ground/build_soong build/soong
 
 echo "========================"
 echo " Starting Build: Vanilla"
 echo "========================"
 
 # Setup untuk perangkat
-lunch lineage_X00TD-user
+#lunch lineage_X00TD-bp4a-user
 make installclean
-mka bacon
+brunch X00TD
+# mka bacon
 
 # Upload VANILLA Build
-for file in out/target/product/X00TD/lineage*.zip; do
+for file in out/target/product/X00TD/voltage*.zip; do
     if [ -f "$file" ]; then
         echo "Mulai mengupload VANILLA: $file"
         curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
