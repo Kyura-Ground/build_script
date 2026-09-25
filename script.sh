66f85bc04404d31a70139f0584e44eb1b17a7e47
M. Ikhwan Fahmi <mikhwanfahmi4@gmail.com>
2026-09-25 13:26:16 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 17 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Evolution-X/manifest -b cnb -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
 rm -rf .repo/local_manifests
-git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Infinity .repo/local_manifests
+git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Evox .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -34,12 +34,12 @@ export TZ="Asia/Jakarta"
 . build/envsetup.sh
 
 # rm -rf vendor/evolution-priv/keys
-git clone --depth=1 https://github.com/VoltageOS/vendor_voltage-priv_keys vendor/voltage-priv/keys
-cd vendor/voltage-priv/keys
+git clone --depth=1 https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
+cd vendor/evolution-priv/keys
 ./keys.sh
 cd ../../..
 
 # Setup device
-lunch infinity_X00TD-userdebug
+lunch lineage_X00TD-cp2a-user
 make installclean
-m bacon -j$(nproc --all)
+m evolution -j$(nproc --all)
