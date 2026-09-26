e90b17a1bd48dc79daf887b2e8bd3cecc7c64c60
M. Ikhwan Fahmi <mikhwanfahmi4@gmail.com>
2026-09-26 08:59:43 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ remove_lists=(
     device/asus/X00TD
     kernel/asus/sdm660
     vendor/asus
+    vendor/voltage-priv/keys
     vendor/evolution-priv/keys
     prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
 )
@@ -18,13 +19,13 @@ echo "-- Removing ${remove_lists[@]}"
 rm -rf "${remove_lists[@]}"
 
 #repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Evolution-X/manifest -b cnb -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 17 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Evox .repo/local_manifests
+git clone --depth=1 https://github.com/Kyura-Ground/local_manifests.git -b Infinity .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -39,12 +40,12 @@ echo "============="
 # FIX KONFLIK MODULE ALREADY DEFINED
 # Dijalankan SETELAH sync agar folder tidak diunduh ulang
 # ==========================================================
-echo "================================================="
-echo " Menghapus folder QCOM-CAF dan Font yang bentrok "
-echo "================================================="
-rm -rf hardware/qcom-caf/sdm845
-rm -rf hardware/qcom-caf/msm8998
-rm -rf packages/overlays/Lineage/fonts
+# echo "================================================="
+# echo " Menghapus folder QCOM-CAF dan Font yang bentrok "
+# echo "================================================="
+# rm -rf hardware/qcom-caf/sdm845
+# rm -rf hardware/qcom-caf/msm8998
+# rm -rf packages/overlays/Lineage/fonts
 
 # Set up build environment
 export BUILD_USERNAME=kyura
@@ -63,12 +64,12 @@ echo " Starting Build: Vanilla"
 echo "========================"
 
 # Setup untuk perangkat
-lunch lineage_X00TD-cp2a-user
+lunch infinity_X00TD-userdebug
 make installclean
-m evolution
+m bacon
 
 # Upload VANILLA Build
-for file in out/target/product/X00TD/EvolutionX*.zip; do
+for file in out/target/product/X00TD/Project_Infinity*.zip; do
     if [ -f "$file" ]; then
         echo "Mulai mengupload VANILLA: $file"
         curl -T "$file" -u :8490fc51-f593-4c87-8e35-3379cf5a94a3 https://pixeldrain.com/api/file/
